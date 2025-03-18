3db7a9f25ad00f9228a6ac97af41bca3040dfc23
Mockingjay-HrGs <noah.dunis@coda-student.school>
2025-03-18 14:28:16 +0100

    interface test

--- src/main.ts
+++ src/main.ts
@@ -4,3 +4,15 @@ import { AppComponent } from './app/app.component';
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
+
+const sidebar = document.getElementById("sidebar");
+
+if (sidebar) {
+  sidebar.addEventListener("mouseenter", () => {
+    sidebar.classList.add("open");
+  });
+
+  sidebar.addEventListener("mouseleave", () => {
+    sidebar.classList.remove("open");
+  });
+}
